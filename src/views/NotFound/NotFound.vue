1bb9cece30607308e017f9f7962962bffe2cc3af
Luis Belo <luzaidinbelo@gmail.com>
2021-01-10 20:36:15 -0300

    feat: add Not Found view

---
+++ src/views/NotFound/NotFound.vue
@@ -0,0 +1,16 @@
+<template>
+    <div style="height:100%; background-size: cover; background-attachment: fixed; background-position: center center;" :style="{ 'background-image': 'url(' + image + ')' }"></div>
+</template>
+
+<script>
+    import Image from '../../assets/closed_404.jpg'
+    export default {
+        name: 'NotFoud',
+
+        data(){
+            return{
+                image: Image
+            }
+        }
+    }
+</script>
