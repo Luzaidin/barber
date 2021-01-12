105a356dbcaa0f9572b182f07b49b8428875c43d
Luis Belo <luzaidinbelo@gmail.com>
2021-01-11 22:22:39 -0300

    refactor: add email and password inputs

--- src/components/Login/Form.vue
+++ src/components/Login/Form.vue
@@ -1,12 +1,66 @@
 <template>
-    <div>
+    <v-card
+    :loading="loading"
+    class="mx-auto my-12"
+    >   
+        <template slot="progress">
+            <v-progress-linear
+                color="deep-blue"
+                height="10"
+                indeterminate
+            ></v-progress-linear>
+        </template>
 
-    </div>
+        <v-card-title class="justify-center">Login</v-card-title>
+
+        <v-card-text>
+            <v-row
+                align="center"
+                class="mx-10"
+            >
+                <v-text-field
+                v-model="form.email.value"
+                :rules="form.email.rule"
+                label="E-mail"
+                required
+                ></v-text-field>
+            </v-row>
+
+            <v-row
+                align="center"
+                class="mx-10 my-10"
+            >
+                <v-text-field
+                v-model="form.password.value"
+                :type="'password'"
+                label="Senha"
+                required
+                ></v-text-field>
+            </v-row>
+        </v-card-text>
+    </v-card>
 </template>
 
 <script>
     export default {
-        
+        name: 'Form',
+        data(){
+            return{
+                loading: false,
+                form: {
+                    email: {
+                        value: '',
+                        rule: [
+                            v => !!v || 'E-mail is required',
+                            v => /.+@.+\..+/.test(v) || 'E-mail must be valid',
+                        ]
+                    },
+                    password: {
+                        value: ''
+                    }
+                }
+            }
+        }
     }
 </script>
 
