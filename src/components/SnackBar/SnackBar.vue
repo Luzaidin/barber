adc497661d40b007da0375c6da09d9e295ad152c
Luis Belo <luzaidinbelo@gmail.com>
2021-02-06 08:47:24 -0300

    feat: add snackBar component

---
+++ src/components/SnackBar/SnackBar.vue
@@ -0,0 +1,19 @@
+<template>
+    <v-snackbar
+    v-model="snackbar"
+    :timeout="timeout"
+    color="error"
+    >
+    Error! E-mail or Password are not valid
+    </v-snackbar>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
