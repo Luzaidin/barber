0af01617bd5c319b4737827b596f770da4deead8
Luis Belo <luzaidinbelo@gmail.com>
2021-02-05 23:02:37 -0300

    feat: add button at center component

---
+++ src/components/Button/ButtonCenter.vue
@@ -0,0 +1,21 @@
+<template>
+    <v-row  
+    class="justify-center">
+        <v-btn
+        color="primary"
+        text
+        >
+            Forgot password?
+        </v-btn>
+    </v-row>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
