2b629ab9c6e6a1630e1ccf688f306cf9cffb31c0
Luis Belo <luzaidinbelo@gmail.com>
2021-02-18 20:58:33 -0300

    feat: update name and color prop

---
+++ src/components/ProgressLinear/ProgressLinear.vue
@@ -0,0 +1,23 @@
+<template slot="progress">
+    <v-progress-linear
+    :color="color"
+    :height="height"
+    indeterminate
+    ></v-progress-linear>
+</template>
+
+<script>
+    export default {
+        name: 'ProgressLinear',
+        data() {
+            return {
+                color: 'blue darken-4',
+                height: '10'
+            }
+        },
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
