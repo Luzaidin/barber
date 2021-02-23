1934d0d5cf6bc83e928bc28e4ece0ef523ef569c
Luis Belo <luzaidinbelo@gmail.com>
2021-02-22 21:12:44 -0300

    refactor: chane home  route

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Home from '../views/Home/Home.vue'
 import Login from '../views/Login/Login.vue'
 import NotFoud from '../views/NotFound/NotFound.vue'
 import SignIn from '../views/SignIn/SignIn.vue'
