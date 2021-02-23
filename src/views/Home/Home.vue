de8e7010aa90045816dfc86734d647b8bd08d427
Luis Belo <luzaidinbelo@gmail.com>
2021-02-22 21:12:11 -0300

    refactor: update  name

---
+++ src/views/Home/Home.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <Calendar />
+    </div>
+</template>
+
+<script>
+import Calendar from '../../components/Calendar/Calendar'
+    export default {
+        name: 'Home',
+        components: {
+            Calendar,
+        },
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
