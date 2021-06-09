3ef4c89f33a3785ce6e4f734ac3ad2d837950fbc
Luis Belo <luzaidinbelo@gmail.com>
2021-06-09 19:14:55 -0300

    feat: add success alert component

---
+++ src/components/Alerts/SuccessAlert.vue
@@ -0,0 +1,23 @@
+<template>
+    <v-alert
+    type="success">
+      {{text}}
+    </v-alert>
+</template>
+
+<script>
+    export default {
+        name: "SuccessAlert",
+        props:{
+            text: {
+                type: String,
+                default: 'Success'
+            },
+        }
+
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
