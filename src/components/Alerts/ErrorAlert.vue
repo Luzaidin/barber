b10c5e494d2c757778d2d48e5a5160d2e01e39fb
Luis Belo <luzaidinbelo@gmail.com>
2021-06-10 20:00:54 -0300

    feat: add errorAlert component

---
+++ src/components/Alerts/ErrorAlert.vue
@@ -0,0 +1,23 @@
+<template>
+    <v-alert
+    type="error">
+      {{text}}
+    </v-alert>
+</template>
+
+<script>
+    export default {
+        name: "ErrorAlert",
+        props:{
+            text: {
+                type: String,
+                default: 'Error'
+            },
+        }
+
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
