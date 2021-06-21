e5ae922a78cf5584a44dd51ffaf1109447d82fc7
Luis Belo <luzaidinbelo@gmail.com>
2021-06-21 19:17:45 -0300

    style: add welcome barber img as background

--- src/views/SignIn/SignIn.vue
+++ src/views/SignIn/SignIn.vue
@@ -1,8 +1,6 @@
 <template>
   <div class="signUpDiv">
     <div class="signupSection">
-      <div class="info">
-      </div>
       <div class="signupForm" name="signupform">
         <Form 
           :SubmitFormBtnName=SubmitFormBtnName 
@@ -50,14 +48,11 @@ import Form from '../../components/Form/Form'
 
 .signUpDiv {
   height: 100%;
-  width: 100%;
-  background: url('../../assets/Login/barber_home.jpg');
-  background-repeat: round;
+  background: url('../../assets/barber-shop-welcome.jpg') no-repeat center center fixed;
+  background-size: cover;
 }
 
 .signupSection {
-  background: url(https://source.unsplash.com/TV2gg2kZD1o/1600x800);
-  background-repeat: no-repeat;
   position: absolute;
   top: 50%;
   left: 50%;
@@ -70,66 +65,11 @@ import Form from '../../components/Form/Form'
   box-shadow: 3px 10px 20px 5px rgba(0, 0, 0, .5);
 }
 
-.info {
-  background: rgba(20, 20, 20, .8);
-  padding: 30px 0;
-  border-right: 5px solid rgba(30, 30, 30, .8);
-  h2 {
-    padding-top: 30px;
-    font-weight: 300;
-  }
-  p {
-    font-size: 18px;
-  }
-  .icon {
-    font-size: 8em;
-    padding: 20px 0;
-    color: rgba(10, 180, 180, 1);
-  }
-}
-
 .signupForm {
-  width: 70%;
+  width: 100%;
   padding: 30px 0;
   background: rgba(20, 40, 40, .8);
   transition: .2s;
-  h2 {
-    font-weight: 300;
-  }
-}
-
-.inputFields {
-  margin: 15px 0;
-  font-size: 16px;
-  padding: 10px;
-  width: 250px;
-  border: 1px solid rgba(10, 180, 180, 1);
-  border-top: none;
-  border-left: none;
-  border-right: none;
-  background: rgba(20, 20, 20, .2);
-  color: white;
-  outline: none;
-}
-
-.noBullet {
-  list-style-type: none;
-  padding: 0;
-}
-
-#join-btn {
-  border: 1px solid rgba(10, 180, 180, 1);
-  background: rgba(20, 20, 20, .6);
-  font-size: 18px;
-  color: white;
-  margin-top: 20px;
-  padding: 10px 50px;
-  cursor: pointer;
-  transition: .4s;
-  &:hover {
-    background: rgba(20, 20, 20, .8);
-    padding: 10px 80px;
-  }
 }
 
 </style>
