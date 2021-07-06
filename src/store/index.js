fbd295ed815d1eff62db1d12f7d06c7e88d55931
Luis Belo <luzaidinbelo@gmail.com>
2021-07-05 22:17:29 -0300

    refactor: ad user setter(mutation)

--- src/store/index.js
+++ src/store/index.js
@@ -10,7 +10,11 @@ export default new Vuex.Store({
       password: ''
     }
   },
-  mutations: {},
+  mutations: {
+    setUser(state, payload) {
+      state.user = payload;
+    }
+  },
   actions: {},
   modules: {},
   getters: {}
