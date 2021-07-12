cf0d7d495c6644bb4915b8105b33ce12a950f6f0
Luis Belo <luzaidinbelo@gmail.com>
2021-07-12 19:50:41 -0300

    feat: add createuser with firebase method

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -1,15 +1,27 @@
 import firebase from firebase
 
-function registerUser(user){
-    firebase
-    .auth()
-    .createUserWithEmailAndPassword(user.email, user.password)
-    .then((res) => {
-        return true
-      //this.$router.push({ name: "Home" });
-    })
-    .catch((error) => {
-        return false
-      //alert(error.message);
-    });
-}
+function registerUser(user) {
+  firebase
+  .auth()
+  .createUserWithEmailAndPassword(user.email, user.password)
+  .then((res) => {
+      return true
+    //this.$router.push({ name: "Home" });
+  })
+  .catch((error) => {
+      return false
+    //alert(error.message);
+  });
+};
+
+function createUser(user) {
+  firebase
+  .auth()
+  .signInWithEmailAndPassword(user.email, user.password)
+  .then(() => {
+      this.$router.push({ name: "Home" });
+  })
+  .catch((error) => {
+      alert(error.message);
+  });
+};
